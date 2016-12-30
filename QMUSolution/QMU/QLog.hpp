677ad103c3df1bf0496362d9cf4d59ffe6d52afd
Austin Quick <daskie.q@gmail.com>
2016-12-30 15:04:41 -0800

    k

--- QMUSolution/QMU/QLog.hpp
+++ QMUSolution/QMU/QLog.hpp
@@ -8,42 +8,38 @@
 
 namespace qmu {
 
-
-
 QMU_API void log(std::ostream & logStream);
 QMU_API std::ostream & log();
-
-
 
 inline void error_break() {}
 inline void warning_break() {}
 inline void exception_break() {}
 
+}
+
 
 
 #define Q_ERROR(msg) do { \
-	QLog::error_break(); \
-	QLog::log() << "ERROR" << std::endl; \
-	QLog::log() << "\t" << msg << std::endl; \
-	QLog::log() << "\t" << __func__ << " (" << __FILE__ << "(" << __LINE__ << "))" << std::endl; \
+	qmu::error_break(); \
+	qmu::log() << "ERROR" << std::endl; \
+	qmu::log() << "\t" << msg << std::endl; \
+	qmu::log() << "\t" << __func__ << " (" << __FILE__ << "(" << __LINE__ << "))" << std::endl; \
 } while (false)
 
 #define Q_WARNING(msg) do { \
-	QLog::warning_break(); \
-	QLog::log() << "WARNING" << std::endl; \
-	QLog::log() << "\t" << msg << std::endl; \
-	QLog::log() << "\t" << __func__ << " (" << __FILE__ << "(" << __LINE__ << "))" << std::endl; \
+	qmu::warning_break(); \
+	qmu::log() << "WARNING" << std::endl; \
+	qmu::log() << "\t" << msg << std::endl; \
+	qmu::log() << "\t" << __func__ << " (" << __FILE__ << "(" << __LINE__ << "))" << std::endl; \
 } while (false)
 
 #define Q_LOG(msg) do { \
-	QLog::log() << msg << std::endl; \
+	qmu::log() << msg << std::endl; \
 } while (false)
 
 #define Q_EXCEPTION(msg) do { \
-	QLog::exception_break(); \
-	QLog::log() << "EXCEPTION" << std::endl; \
-	QLog::log() << "\t" << msg << std::endl; \
-	QLog::log() << "\t" << __func__ << " (" << __FILE__ << "(" << __LINE__ << "))" << std::endl; \
+	qmu::exception_break(); \
+	qmu::log() << "EXCEPTION" << std::endl; \
+	qmu::log() << "\t" << msg << std::endl; \
+	qmu::log() << "\t" << __func__ << " (" << __FILE__ << "(" << __LINE__ << "))" << std::endl; \
 } while (false)
-
-}
