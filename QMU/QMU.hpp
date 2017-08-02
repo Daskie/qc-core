66c614d055cf67bf045667237334553a35361006
Austin Quick <daskie.q@gmail.com>
2017-08-02 16:48:41 -0700

    separated out qmu implementation

--- QMU/QMU.hpp
+++ QMU/QMU.hpp
@@ -92,16 +92,16 @@ using eif_integral_t = eif_t<std::is_integral<T>::value>;
 namespace detail {
 
 template <typename T, eif_floating_t<T> = 0>
-constexpr T sqrt_constexpr_helper(T v, T curr, T prev) {
+constexpr T sqrtConstexprHelper(T v, T curr, T prev) {
     if (curr == prev) {
         return curr;
     }
 
-    return sqrt_constexpr_helper(v, static_cast<T>(0.5) * (curr + v / curr), curr);
+    return sqrtConstexprHelper(v, static_cast<T>(0.5) * (curr + v / curr), curr);
 }
 
 template <typename T, eif_floating_t<T> = 0>
-constexpr T sqrt_constexpr(T v) {
+constexpr T sqrtConstexpr(T v) {
     if (v == static_cast<T>(0.0) || v == std::numeric_limits<T>::infinity()) {
         return v;
     }
@@ -109,7 +109,7 @@ constexpr T sqrt_constexpr(T v) {
         return std::numeric_limits<T>::quiet_NaN();
     }
 
-    return sqrt_constexpr_helper(v, v, static_cast<T>(0.0));
+    return sqrtConstexprHelper(v, v, static_cast<T>(0.0));
 }
 
 }
@@ -129,8 +129,126 @@ template <typename T, eif_floating_t<T> = 0> constexpr T  pi = static_cast<T>(3.
 template <typename T, eif_floating_t<T> = 0> constexpr T   e = static_cast<T>(2.71828182845904523536028747135266250L);
 template <typename T, eif_floating_t<T> = 0> constexpr T phi = static_cast<T>(1.61803398874989484820458683436563812L);
 
-template <typename T, nat t_v, eif_floating_t<T> = 0> constexpr T sqrt = detail::sqrt_constexpr(static_cast<T>(t_v));
+template <typename T, nat t_v, eif_floating_t<T> = 0> constexpr T sqrt = detail::sqrtConstexpr(static_cast<T>(t_v));
 template <typename T, eif_floating_t<T> = 0> constexpr T infinity = std::numeric_limits<T>::infinity();
+
+
+
+template <typename T>
+constexpr const T & min(const T & a, const T & b);
+
+template <typename T, typename... Ts>
+constexpr const T & min(const T & a, const T & b, const Ts &... rest);
+
+template <typename T>
+constexpr const T & max(const T & a, const T & b);
+
+template <typename T, typename... Ts>
+constexpr const T & max(const T & a, const T & b, const Ts &... rest);
+
+template <typename T>
+constexpr T clamp(const T & v, const T & min, const T & max);
+
+// similar performance to std but allows constexpr
+template <typename T, eif_t<std::is_unsigned<T>::value> = 0>
+constexpr T abs(T v);
+
+// similar performance to std but allows constexpr
+template <typename T, eif_t<std::is_signed<T>::value> = 0>
+constexpr T abs(T v);
+
+template <typename T, eif_floating_t<T> = 0>
+constexpr bool zero(T v, T e = std::numeric_limits<T>::min());
+
+template <typename T, eif_integral_t<T> = 0>
+constexpr bool zero(T v);
+
+template <typename T, eif_floating_t<T> = 0>
+constexpr bool equal(T v1, T v2);
+
+template <typename T, eif_t<!std::is_floating_point<T>::value> = 0>
+constexpr bool equal(const T & v1, const T & v2);
+
+template <typename T, typename... Ts>
+constexpr bool equal(const T & v1, const T & v2, const Ts &... rest);
+
+template <typename T, eif_t<std::is_signed<T>::value> = 0>
+constexpr T sign(T v);
+
+template <typename T, eif_t<std::is_unsigned<T>::value> = 0>
+constexpr T sign(T v);
+
+// ~2x faster than std::floor
+template <typename T, eif_floating_t<T> = 0>
+constexpr nat floor(T v);
+
+template <typename T, eif_integral_t<T> = 0>
+constexpr T floor(T v);
+
+// ~2x faster than std::ceil
+template <typename T, eif_floating_t<T> = 0>
+constexpr nat ceil(T v);
+
+template <typename T, eif_integral_t<T> = 0>
+constexpr T ceil(T v);
+
+template <typename T, eif_integral_t<T> = 0>
+constexpr match_sign_t<nat, T> log2(T v);
+
+template <typename T, eif_floating_t<T> = 0>
+constexpr T log2(T v);
+
+template <typename T, eif_integral_t<T> = 0>
+constexpr bool isPow2(T v);
+
+template <typename T, eif_integral_t<T> = 0>
+constexpr T floor2(T v);
+
+template <typename T, eif_integral_t<T> = 0>
+constexpr T ceil2(T v);
+
+template <typename T, eif_integral_t<T> = 0>
+constexpr T highBit(T v);
+
+template <typename T, eif_integral_t<T> = 0>
+constexpr T lowBit(T v);
+
+// ~3.3x faster than std::modf
+template <typename T, eif_floating_t<T> = 0>
+T fract(T v, nat & i);
+
+template <typename T, eif_floating_t<T> = 0>
+constexpr T fract(T v);
+
+// ~2.5x faster than std::fmod
+template <typename T, eif_floating_t<T> = 0>
+T mod(T v, T d, T & q);
+
+template <typename T, eif_floating_t<T> = 0>
+constexpr T mod(T v, T d);
+
+template <typename T, eif_integral_t<T> = 0>
+T mod(T v, T d, T & q);
+
+template <typename T, eif_integral_t<T> = 0>
+constexpr T mod(T v, T d);
+
+template <typename T, eif_floating_t<T> = 0>
+constexpr T mix(T v1, T v2, T t);
+
+template <typename T, eif_floating_t<T> = 0>
+constexpr T radians(T degrees);
+
+template <typename T, eif_floating_t<T> = 0>
+constexpr T degrees(T radians);
+
+
+
+//==============================================================================================================================================================
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// IMPLEMENTATION ==============================================================================================================================================
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//==============================================================================================================================================================
 
 
 
@@ -159,14 +277,12 @@ constexpr T clamp(const T & v, const T & min, const T & max) {
     return qmu::min(qmu::max(v, min), max);
 }
 
-// similar performance to std but allows constexpr
-template <typename T, eif_t<std::is_unsigned<T>::value> = 0>
+template <typename T, eif_t<std::is_unsigned<T>::value>>
 constexpr T abs(T v) {
     return v; 
 }
 
-// similar performance to std but allows constexpr
-template <typename T, eif_t<std::is_signed<T>::value> = 0>
+template <typename T, eif_t<std::is_signed<T>::value>>
 constexpr T abs(T v) {
     return v < static_cast<T>(0) ? -v : v;
 }
@@ -183,22 +299,22 @@ constexpr T abs(T v) {
 }
 */
 
-template <typename T, eif_floating_t<T> = 0>
-constexpr bool zero(T v, T e = std::numeric_limits<T>::min()) {
+template <typename T, eif_floating_t<T>>
+constexpr bool zero(T v, T e) {
     return abs(v) < e;
 }
 
-template <typename T, eif_integral_t<T> = 0>
+template <typename T, eif_integral_t<T>>
 constexpr bool zero(T v) {
     return v == static_cast<T>(0);
 }
 
-template <typename T, eif_floating_t<T> = 0>
+template <typename T, eif_floating_t<T>>
 constexpr bool equal(T v1, T v2) {
     return zero(v1 - v2, std::numeric_limits<T>::min());
 }
 
-template <typename T, eif_t<!std::is_floating_point<T>::value> = 0>
+template <typename T, eif_t<!std::is_floating_point<T>::value>>
 constexpr bool equal(const T & v1, const T & v2) {
     return v1 == v2;
 }
@@ -208,41 +324,39 @@ constexpr bool equal(const T & v1, const T & v2, const Ts &... rest) {
     return equal(v1, v2) && equal(v2, rest...);
 }
 
-template <typename T, eif_t<std::is_signed<T>::value> = 0>
+template <typename T, eif_t<std::is_signed<T>::value>>
 constexpr T sign(T v) {
     return static_cast<T>(static_cast<T>(0) < v) - static_cast<T>(v < static_cast<T>(0));
 }
 
-template <typename T, eif_t<std::is_unsigned<T>::value> = 0>
+template <typename T, eif_t<std::is_unsigned<T>::value>>
 constexpr T sign(T v) {
     return static_cast<T>(v > static_cast<T>(0));
 }
 
-// ~2x faster than std::floor
-template <typename T, eif_floating_t<T> = 0>
+template <typename T, eif_floating_t<T>>
 constexpr nat floor(T v) {
     nat i(static_cast<nat>(v));
     return i - (v < i);
 }
 
-template <typename T, eif_integral_t<T> = 0>
+template <typename T, eif_integral_t<T>>
 constexpr T floor(T v) {
     return v;
 }
 
-// ~2x faster than std::ceil
-template <typename T, eif_floating_t<T> = 0>
+template <typename T, eif_floating_t<T>>
 constexpr nat ceil(T v) {
     nat i(static_cast<nat>(v));
     return i + (v > i);
 }
 
-template <typename T, eif_integral_t<T> = 0>
+template <typename T, eif_integral_t<T>>
 constexpr T ceil(T v) {
     return v;
 }
 
-template <typename T, eif_integral_t<T> = 0>
+template <typename T, eif_integral_t<T>>
 constexpr match_sign_t<nat, T> log2(T v) {
     match_sign_t<nat, T> log(0);
     if (sizeof(T) >= 8) {
@@ -260,29 +374,29 @@ constexpr match_sign_t<nat, T> log2(T v) {
     return log;
 }
 
-template <typename T, eif_floating_t<T> = 0>
+template <typename T, eif_floating_t<T>>
 constexpr T log2(T v) {
     return std::log2(v);
 }
 
-template <typename T, eif_integral_t<T> = 0>
+template <typename T, eif_integral_t<T>>
 constexpr bool isPow2(T v) {
     return (v & (v - 1)) == 0;
 }
 
-template <typename T, eif_integral_t<T> = 0>
+template <typename T, eif_integral_t<T>>
 constexpr T floor2(T v) {
     if (v == 0) return 0;
     return T(1) << log2(v);
 }
 
-template <typename T, eif_integral_t<T> = 0>
+template <typename T, eif_integral_t<T>>
 constexpr T ceil2(T v) {
     if (v == 0) return 0;
     return floor2(v * 2 - 1);
 }
 
-template <typename T, eif_integral_t<T> = 0>
+template <typename T, eif_integral_t<T>>
 constexpr T highBit(T v) {
     using UT = std::make_unsigned_t<T>;
     constexpr UT mask(static_cast<UT>(1) << (sizeof(T) * 8 - 1));
@@ -290,58 +404,56 @@ constexpr T highBit(T v) {
     return static_cast<T>(static_cast<UT>(v) & mask);
 }
 
-template <typename T, eif_integral_t<T> = 0>
+template <typename T, eif_integral_t<T>>
 constexpr T lowBit(T v) {
     return v & static_cast<T>(1);
 }
 
-// ~3.3x faster than std::modf
-template <typename T, eif_floating_t<T> = 0>
+template <typename T, eif_floating_t<T>>
 inline T fract(T v, nat & i) {
     i = static_cast<nat>(v);
     return v - i;
 }
 
-template <typename T, eif_floating_t<T> = 0>
+template <typename T, eif_floating_t<T>>
 constexpr T fract(T v) {
     return v - static_cast<nat>(v);
 }
 
-// ~2.5x faster than std::fmod
-template <typename T, eif_floating_t<T> = 0>
+template <typename T, eif_floating_t<T>>
 inline T mod(T v, T d, T & q) {
     q = v / d;
     return fract(q) * d;
 }
 
-template <typename T, eif_floating_t<T> = 0>
+template <typename T, eif_floating_t<T>>
 constexpr T mod(T v, T d) {
     return fract(v / d) * d;
 }
 
-template <typename T, eif_integral_t<T> = 0>
+template <typename T, eif_integral_t<T>>
 inline T mod(T v, T d, T & q) {
     d = abs(d);
     q = v / d;
     return v - q * d;
 }
 
-template <typename T, eif_integral_t<T> = 0>
+template <typename T, eif_integral_t<T>>
 constexpr T mod(T v, T d) {
     return v % d;
 }
 
-template <typename T, eif_floating_t<T> = 0>
+template <typename T, eif_floating_t<T>>
 constexpr T mix(T v1, T v2, T t) {
     return (static_cast<T>(1.0) - t) * v1 + t * v2;
 }
 
-template <typename T, eif_floating_t<T> = 0>
+template <typename T, eif_floating_t<T>>
 constexpr T radians(T degrees) {
     return degrees * pi<T> / static_cast<T>(180.0);
 }
 
-template <typename T, eif_floating_t<T> = 0>
+template <typename T, eif_floating_t<T>>
 constexpr T degrees(T radians) {
     return radians * static_cast<T>(180.0) / pi<T>;
 }
