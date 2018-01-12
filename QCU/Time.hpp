33ad8f48d236dad8e35e53cfcf27b67a5b302212
Austin Quick <daskie.q@gmail.com>
2018-01-11 19:23:11 -0800

    touched up clock a bit

--- QCU/Time.hpp
+++ QCU/Time.hpp
@@ -12,10 +12,15 @@ namespace qcu {
 
 
 
+// seconds since epoch
 double now();
 
 
 
+// Simple clock usable for most timing-related needs
+// Period is the number of seconds per clock cycle
+// Frequency is how many cycles happen per seconds
+// Period = 1 / Frequency
 class Clock {
 
     private:
@@ -26,24 +31,31 @@ class Clock {
 
     public:
 
+    // constructs clock with 1.0 sec period and 1.0 sec frequency
     Clock();
 
+    // the clock starts now
     void restart();
+    // the clock starts at the given time in seconds since epoch
+    void restart(double t);
 
+    // the fractional number of cycles since the clock started
     double age() const;
+
+    // the integral number or cycles since the clock started
+    nat cycles() const;
     
+    // the fractional part of the current cycle
     double time() const;
 
-    long long cycles() const;
-
-    void recycle();
-
-    void decycle();
-
+    // get the period
     double period() const;
+    // set the period
     void period(double period);
 
+    // get the frequency
     double frequency() const;
+    // set the frequency
     void frequency(double frequency);
 
 };
@@ -72,26 +84,20 @@ void Clock::restart() {
     m_start = std::chrono::high_resolution_clock::now();
 }
 
-double Clock::age() const {
-    return std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now() - m_start).count() * m_frequency;
+void Clock::restart(double t) {
+    m_start = std::chrono::high_resolution_clock::time_point(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<double>(1.0)));
 }
 
-double Clock::time() const {
-    return fract(age());
+double Clock::age() const {
+    return std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now() - m_start).count() * m_frequency;
 }
 
 nat Clock::cycles() const {
     return nat(age());
 }
 
-void Clock::recycle() {
-    m_start += std::chrono::duration_cast<std::chrono::high_resolution_clock::duration>(std::chrono::duration<double>(cycles() * m_period));
-}
-
-void Clock::decycle() {
-    if (cycles() > 0) {
-        m_start += std::chrono::duration_cast<std::chrono::high_resolution_clock::duration>(std::chrono::duration<double>(m_period));
-    }
+double Clock::time() const {
+    return fract(age());
 }
 
 double Clock::period() const {
