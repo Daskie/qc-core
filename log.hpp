d81cefdff29fa534775610d0549e03f6b8d7c494
Austin Quick <daskie.q@gmail.com>
2020-05-25 23:23:00 -0600

    minor log updates

--- log.hpp
+++ log.hpp
@@ -14,27 +14,25 @@ namespace qc::log {
 }
 
 #define QC_ERROR(msg) do { \
-    std::cout << "ERROR" << std::endl; \
-    std::cout << "\t" << msg << std::endl; \
-    std::cout << "\t" << __func__ << " (" << __FILE__ << "(" << __LINE__ << "))" << std::endl; \
+    std::cerr << "ERROR [" << __FILE__ << ":" << __LINE__ << " " << __func__ << "]: " << msg << std::endl; \
     qc::log::error_break(); \
 } while (false)
 
 #define QC_WARNING(msg) do { \
-    std::cout << "WARNING" << std::endl; \
-    std::cout << "\t" << msg << std::endl; \
-    std::cout << "\t" << __func__ << " (" << __FILE__ << "(" << __LINE__ << "))" << std::endl; \
+    std::cerr << "WARNING [" << __FILE__ << ":" << __LINE__ << " " << __func__ << "]: " << msg << std::endl; \
     qc::log::warning_break(); \
 } while (false)
 
-#define QC_LOG(msg) do { \
-    std::cout << msg << std::endl; \
+#define QC_INFO(msg) do { \
+    std::cerr << "INFO [" << __FILE__ << ":" << __LINE__ << " " << __func__ << "]: " << msg << std::endl; \
+} while (false)
+
+#define QC_DEBUG(msg) do { \
+    std::cerr << "DEBUG [" << __FILE__ << ":" << __LINE__ << " " << __func__ << "]: " << msg << std::endl; \
 } while (false)
 
 #define QC_EXCEPTION(msg, exception) do { \
-    std::cout << "EXCEPTION" << std::endl; \
-    std::cout << "\t" << msg << std::endl; \
-    std::cout << "\t" << __func__ << " (" << __FILE__ << "(" << __LINE__ << "))" << std::endl; \
+    std::cerr << "EXCEPTION [" << __FILE__ << ":" << __LINE__ << " " << __func__ << "]: " << msg << std::endl; \
     qc::log::exception_break(); \
     throw exception; \
 } while (false)
