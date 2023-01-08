dfcdf096cf4bc8cc262d10b34b3fdb48bd649458
Austin Quick <daskie.q@gmail.com>
2023-01-08 00:12:32 -0800

    random: Using SFC PRNG

--- include/qc-core/random.hpp
+++ include/qc-core/random.hpp
@@ -1,78 +1,63 @@
 #pragma once
 
+#include <bit>
 #include <chrono>
-#include <random>
 
 #include <qc-core/core.hpp>
 
 namespace qc
 {
     ///
-    /// Random number generator which uses std::mt19937/std::mt19937_64
+    /// Uses the SFC pseudo-random number generator
+    ///   - https://gist.github.com/imneme/f1f7821f07cf76504a97f6537c818083
+    ///   - https://www.pcg-random.org/posts/some-prng-implementations.html
+    ///   - https://pracrand.sourceforge.net/
     ///
-    /// This is a rather large object (~ 5 KB), but each call to `next` is very fast, so construct once and use a bunch
-    ///
+    template <UnsignedIntegral G = unat>
     class Random
     {
       public:
 
-        using Engine = std::conditional_t<sizeof(void *) <= 4u, std::mt19937, std::mt19937_64>;
-
         ///
         /// Constructs a new random generater seeded with the given seed
+        /// @param seed the seed to initialize the engine with
         ///
-        /// @param seed the seed to initialize the engine with, defaults to the current time
-        ///
-        explicit Random(const unat seed = std::chrono::high_resolution_clock::now().time_since_epoch().count()) noexcept :
-            _seed{seed},
-            _engine{_seed}
-        {}
-
-        Random(const Random & other) noexcept = default;
-
-        Random & operator=(const Random & other) noexcept = default;
-
-        ~Random() noexcept = default;
+        Random() noexcept;
+        explicit Random(const G seed) noexcept;
 
         ///
-        /// @return the next random unsigned integer in [0, `UINT_MAX`]
+        /// @return the next random integer in [0, 2^T_bits), floater in [0.0, 1.0), or boolean
         ///
-        unat next() noexcept;
+        template <NumericOrBoolean T = G> T next() noexcept;
 
         ///
-        /// @return the next random integer in [0, `UINT_MAX`], floater in [0.0, 1.0], or boolean
-        ///
-        template <NumericOrBoolean T> T next() noexcept;
-
-        ///
-        /// If `T` is integral, `max` should be less than `UINT_MAX` by a few orders of magnitude for best results
-        ///
-        /// @return the next random integer in [0, `max`) or floater in [0, `max`]
+        /// @return the next random number in [0, `max`)
         ///
         template <Numeric T> T next(const T max) noexcept;
 
         ///
-        /// If `T` is integral, `max - min` should be less than `UINT_MAX` by a few orders of magnitude for best results
-        ///
-        /// @return the next random integer in [`min`, `max`) or floater in [`min`, `max`]
+        /// @return the next random number in [`min`, `max`)
         ///
         template <Numeric T> T next(const T min, const T max) noexcept;
 
         ///
         /// @return the seed
         ///
-        unat seed() const noexcept { return _seed; }
-
-        ///
-        /// @return the engine
-        ///
-        Engine & engine() noexcept { return _engine; }
-        const Engine & engine() const noexcept { return _engine; }
+        G seed() const noexcept { return _seed; }
 
       private:
 
-        unat _seed{};
-        Engine _engine{};
+        template <typename> struct _Constants;
+        template <> struct _Constants< u8> { static constexpr int p{ 3}, q{ 2}, r{1}; };
+        template <> struct _Constants<u16> { static constexpr int p{ 4}, q{ 3}, r{2}; };
+        template <> struct _Constants<u32> { static constexpr int p{21}, q{ 9}, r{3}; };
+        template <> struct _Constants<u64> { static constexpr int p{24}, q{11}, r{3}; };
+        static constexpr _Constants<G> _constants{};
+
+        inline static std::atomic<G> _globalSeed{G(std::chrono::high_resolution_clock::now().time_since_epoch().count())};
+
+        G _seed{};
+        struct { G a, b, c, d; } _state{};
     };
 }
 
@@ -80,25 +65,40 @@ namespace qc
 
 namespace qc
 {
-    inline unat Random::next() noexcept
+    template <UnsignedIntegral G>
+    inline Random<G>::Random() noexcept :
+        Random(_globalSeed++)
+    {}
+
+    template <UnsignedIntegral G>
+    inline Random<G>::Random(const G seed) noexcept :
+        _seed{seed},
+        _state{.a = _seed, .b = _seed, .c = _seed, .d = 1}
     {
-        return _engine();
+        // Warm up generator
+        for (int i{0}; i < 12; ++i) next();
     }
 
+    template <UnsignedIntegral G>
     template <NumericOrBoolean T>
-    inline T Random::next() noexcept
+    inline T Random<G>::next() noexcept
     {
         if constexpr (Integral<T>)
         {
-            if constexpr (sizeof(T) == sizeof(unat))
+            if constexpr (sizeof(T) == sizeof(G))
             {
-                return T(next());
+                const G tmp{G(_state.a + _state.b + _state.d)};
+                ++_state.d;
+                _state.a = G(_state.b ^ G(_state.b >> _constants.q));
+                _state.b = G(_state.c + G(_state.c << _constants.r));
+                _state.c = G(std::rotl(_state.c, _constants.p) + tmp);
+                return T(tmp);
             }
-            else if constexpr (sizeof(T) < sizeof(unat))
+            else if constexpr (sizeof(T) < sizeof(G))
             {
-                return T(std::make_unsigned_t<T>(next() >> (std::numeric_limits<unat>::digits - std::numeric_limits<T>::digits)));
+                return T(std::make_unsigned_t<T>(next() >> (std::numeric_limits<G>::digits - std::numeric_limits<T>::digits)));
             }
-            else
+            else if constexpr (sizeof(T) > sizeof(G))
             {
                 using U = std::make_unsigned_t<T>;
                 using H = typename sized<sizeof(U) / 2u>::utype;
@@ -117,19 +117,16 @@ namespace qc
             {
                 return T(double(next<u64>() >> 11) * 0x1.0p-53);
             }
-            else
-            {
-                static_assert(!sizeof(T));
-            }
         }
-        else
+        else if constexpr (std::is_same_v<T, bool>)
         {
-            return next() >> (std::numeric_limits<unat>::digits - 1);
+            return next() >> (std::numeric_limits<G>::digits - 1);
         }
     }
 
+    template <UnsignedIntegral G>
     template <Numeric T>
-    inline T Random::next(const T max) noexcept
+    inline T Random<G>::next(const T max) noexcept
     {
         if constexpr (Integral<T>)
         {
@@ -159,8 +156,9 @@ namespace qc
         }
     }
 
+    template <UnsignedIntegral G>
     template <Numeric T>
-    inline T Random::next(const T min, const T max) noexcept
+    inline T Random<G>::next(const T min, const T max) noexcept
     {
         return T(next<T>(T(max - min)) + min);
     }
--- test/test-random.cpp
+++ test/test-random.cpp
@@ -8,176 +8,204 @@ namespace
 {
     constexpr unat n{1000u};
 
-    template <typename T>
-    void testUnsigned(qc::Random & random)
+    template <typename T, typename G>
+    void testUnsigned(qc::Random<G> & random)
     {
+        random.template next<T>();
+
+        {
+            const T v{random.template next<T>(0u)};
+            ASSERT_EQ(v, 0u);
+        }
+
+        {
+            const T v{random.template next<T>(1u)};
+            ASSERT_EQ(v, 0u);
+        }
+
+        u32 bits{};
+
         for (unat i{0u}; i < n; ++i)
         {
             {
-                #pragma warning(suppress: 4189)
-                const T v{random.next<T>()};
-            }
-
-            {
-                const T v{random.next<T>(0u)};
-                ASSERT_EQ(v, 0u);
-            }
-
-            {
-                const T v{random.next<T>(1u)};
-                ASSERT_EQ(v, 0u);
-            }
-
-            {
-                const T v{random.next<T>(2u)};
+                const T v{random.template next<T>(2u)};
                 ASSERT_LT(v, 2u);
             }
 
             {
-                const T v{random.next<T>(3u)};
+                const T v{random.template next<T>(3u)};
                 ASSERT_LT(v, 3u);
             }
 
             {
-                const T v{random.next<T>(4u)};
+                const T v{random.template next<T>(4u)};
                 ASSERT_LT(v, 4u);
             }
 
             {
-                const T v{random.next<T>(5u)};
+                const T v{random.template next<T>(5u)};
                 ASSERT_LT(v, 5u);
             }
 
             {
-                const T v{random.next<T>(3u, 7u)};
+                const T v{random.template next<T>(3u, 7u)};
                 ASSERT_GE(v, 3u);
                 ASSERT_LT(v, 7u);
             }
+
+            bits |= 1u << random.template next<T>(32u);
         }
+
+        ASSERT_EQ(bits, ~u32{});
     }
 
-    template <typename T>
-    void testSigned(qc::Random & random)
+    template <typename T, typename G>
+    void testSigned(qc::Random<G> & random)
     {
         for (unat i{0u}; i < n; ++i)
         {
             {
                 #pragma warning(suppress: 4189)
-                const T v{random.next<T>()};
+                const T v{random.template next<T>()};
             }
 
             {
-                const T v{random.next<T>(0)};
+                const T v{random.template next<T>(0)};
                 ASSERT_EQ(v, 0);
             }
 
             {
-                const T v{random.next<T>(1)};
+                const T v{random.template next<T>(1)};
                 ASSERT_EQ(v, 0);
             }
 
             {
-                const T v{random.next<T>(2)};
+                const T v{random.template next<T>(2)};
                 ASSERT_GE(v, 0);
                 ASSERT_LT(v, 2);
             }
 
             {
-                const T v{random.next<T>(3)};
+                const T v{random.template next<T>(3)};
                 ASSERT_GE(v, 0);
                 ASSERT_LT(v, 3);
             }
 
             {
-                const T v{random.next<T>(4)};
+                const T v{random.template next<T>(4)};
                 ASSERT_GE(v, 0);
                 ASSERT_LT(v, 4);
             }
 
             {
-                const T v{random.next<T>(5)};
+                const T v{random.template next<T>(5)};
                 ASSERT_GE(v, 0);
                 ASSERT_LT(v, 5);
             }
 
             {
-                const T v{random.next<T>(3u, 7u)};
+                const T v{random.template next<T>(3u, 7u)};
                 ASSERT_GE(v, 3u);
                 ASSERT_LT(v, 7u);
             }
         }
     }
 
-    template <typename T>
-    void testFloating(qc::Random & random)
+    template <typename T, typename G>
+    void testFloating(qc::Random<G> & random)
     {
         for (unat i{0u}; i < n; ++i)
         {
             {
-                const T v{random.next<T>()};
+                const T v{random.template next<T>()};
                 ASSERT_GE(v, T(0.0));
                 ASSERT_LT(v, T(1.0));
             }
 
             {
-                const T v{random.next<T>(T(3.25))};
+                const T v{random.template next<T>(T(3.25))};
                 ASSERT_GE(v, T(0.0));
                 ASSERT_LT(v, T(3.25));
             }
 
             {
-                const T v{random.next<T>(T(-3.25))};
+                const T v{random.template next<T>(T(-3.25))};
                 ASSERT_LE(v, T(0.0));
                 ASSERT_GT(v, T(-3.25));
             }
 
             {
-                const T v{random.next<T>(T(3.25), T(7.75))};
+                const T v{random.template next<T>(T(3.25), T(7.75))};
                 ASSERT_GE(v, T(3.25));
                 ASSERT_LT(v, T(7.75));
             }
 
             {
-                const T v{random.next<T>(T(-3.25), T(-7.75))};
+                const T v{random.template next<T>(T(-3.25), T(-7.75))};
                 ASSERT_LE(v, T(-3.25));
                 ASSERT_GT(v, T(-7.75));
             }
 
             {
-                const T v{random.next<T>(T(-3.25), T(7.75))};
+                const T v{random.template next<T>(T(-3.25), T(7.75))};
                 ASSERT_GE(v, T(-3.25));
                 ASSERT_LT(v, T(7.75));
             }
 
             {
-                const T v{random.next<T>(T(3.25), T(-7.75))};
+                const T v{random.template next<T>(T(3.25), T(-7.75))};
                 ASSERT_GE(v, T(-7.75));
                 ASSERT_LT(v, T(3.25));
             }
         }
     }
+
+    template <typename G>
+    void testGeneratorType()
+    {
+        qc::Random<G> random{};
+
+        random.template next<bool>();
+
+        random.next();
+        random.next(10u);
+        random.next(10u, 20u);
+
+        testUnsigned<u8>(random);
+        testUnsigned<u16>(random);
+        testUnsigned<u32>(random);
+        testUnsigned<u64>(random);
+
+        testSigned<s8>(random);
+        testSigned<s16>(random);
+        testSigned<s32>(random);
+        testSigned<s64>(random);
+
+        testFloating<f32>(random);
+        testFloating<f64>(random);
+    }
 }
 
 TEST(Random, types)
 {
-    qc::Random random{};
+    testGeneratorType<u8>();
+    testGeneratorType<u16>();
+    testGeneratorType<u32>();
+    testGeneratorType<u64>();
+}
 
-    random.next<bool>();
+TEST(Random, seed)
+{
+    qc::Random r1{0u};
+    ASSERT_EQ(r1.seed(), 0u);
 
-    random.next();
-    random.next(10u);
-    random.next(10u, 20u);
+    qc::Random r2{17u};
+    ASSERT_EQ(r2.seed(), 17u);
 
-    testUnsigned<u8>(random);
-    testUnsigned<u16>(random);
-    testUnsigned<u32>(random);
-    testUnsigned<u64>(random);
+    ASSERT_NE(r1.next(), r2.next());
 
-    testSigned<s8>(random);
-    testSigned<s16>(random);
-    testSigned<s32>(random);
-    testSigned<s64>(random);
+    qc::Random r3{};
+    qc::Random r4{};
 
-    testFloating<f32>(random);
-    testFloating<f64>(random);
+    ASSERT_NE(r3.seed(), r4.seed());
 }
